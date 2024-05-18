27e70d1128dd399b8ad151b0c401706cfcef902f
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-05-18 19:24:20 +0900

    [level 1] Title: 소수 찾기, Time: 2959.20 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,18 +1,9 @@
-import math
-
 def solution(n):
-    answer = []
-    array = [True for i in range(n + 1)]
-
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if array[i] == True:
-            j = 2
-            while i * j <= n:
-                array[i * j] = False
-                j += 1
-
-    for i in range(2, n + 1):
-        if array[i]:
-            answer.append(i)
-
-    return len(answer)
+    answer = 0
+    for i in range(2, n+1):
+        for j in range(2, int(i**0.5)+1):
+            if i % j == 0:
+                break
+        else:
+            answer += 1
+    return answer
