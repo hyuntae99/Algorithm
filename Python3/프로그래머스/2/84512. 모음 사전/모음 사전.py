3d91459b90130874c573e4bd5dea85a6f8603ad4
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-05-23 20:08:35 +0900

    [level 2] Title: 모음 사전, Time: 3.24 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,29 @@
+import itertools
+
+def solution(word):
+    arr = [] # 만들 수 있는 모든 사전 조합
+    words = ['A', 'E', 'I', 'O', 'U'] # 사용 가능한 문자
+    
+    # 1글자부터 5글자까지의 중복 순열 생성
+    for j in range(1, 6):
+        for i in itertools.product(words, repeat = j):
+            # 생성된 조합을 리스트에 추가
+            arr.append(list(i))
+    
+    # 사전 순서로 정렬
+    arr.sort()
+    
+
+    answer = 0
+    # 모든 조합을 확인하며 단어를 찾기
+    for i in arr:
+        answer += 1
+        # 리스트를 문자열로 변환하여 비교
+        st = ''
+        for s in i:
+            st += s
+        if st == word:
+            break
+    
+    return answer
+
