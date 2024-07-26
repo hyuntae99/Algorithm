4c278b842d4353bb1392d892f6c3f35f9ed11f2c
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-07-26 14:05:25 +0900

    Update 짝지어 제거하기.py

--- Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -5,8 +5,9 @@ def solution(s):
     for i in s:
         # 스택이 비어있지 않고, 스택의 top에 있는 문자와 현재 문자가 같다면
         if stack and stack[-1] == i:
-            stack.pop()
+            stack.pop() # 제거
             continue
+        # 문자와 다르다면 추가
         stack.append(i)
     
     if not stack:
