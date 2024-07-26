a71dec0d356b27a8e009c9dd1611981ead93d8e2
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-07-26 14:04:18 +0900

    [level 2] Title: 짝지어 제거하기, Time: 108.04 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = 0
+    
+    stack = []
+    for i in s:
+        # 스택이 비어있지 않고, 스택의 top에 있는 문자와 현재 문자가 같다면
+        if stack and stack[-1] == i:
+            stack.pop()
+            continue
+        stack.append(i)
+    
+    if not stack:
+        answer = 1
+    return answer
