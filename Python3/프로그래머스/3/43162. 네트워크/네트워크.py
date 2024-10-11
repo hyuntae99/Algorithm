a0ed3de917b784ce5556fc5bd6020ec4c18006cb
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-10-12 08:30:35 +0900

    [level 3] Title: 네트워크, Time: 0.59 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,25 +1,26 @@
 from collections import deque
 
 def solution(n, computers):
-    visited = [False] * n
+    v = [0] * n
     num_networks = 0
     
     # 모든 컴퓨터에 대해서
     for i in range(n):
         # 방문하지 않았다면
-        if not visited[i]:
-            bfs(i, n, computers, visited)
-            print('끝')
+        if not v[i]:
+            bfs(i, n, computers, v)
             num_networks += 1
     
     return num_networks
 
-def bfs(start, n, computers, visited):
-    queue = deque([start])
-    while queue:
-        node = queue.popleft() # 현재 노드
-        for neighbor in range(n):
+def bfs(start, n, computers, v):
+    q = deque()
+    q.append(start)
+    v[start] = 1
+    while q:
+        c = q.popleft() # 현재 노드
+        for i in range(n):
             # 현재 노드의 주변 노드가 1이고 방문하지 않을 때
-            if computers[node][neighbor] == 1 and not visited[neighbor]:
-                visited[neighbor] = True # 방문 처리
-                queue.append(neighbor) # 인접 노드 큐에 추가
+            if computers[c][i] == 1 and not v[i]:
+                v[i] = True # 방문 처리
+                q.append(i) # 인접 노드 큐에 추가
