64b33cc0235c3a5570df0c058ace824b11bf6e64
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-10-12 09:16:22 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ MySQL/프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -2,3 +2,6 @@
 SELECT COUNT(*) AS COUNT
 FROM ECOLI_DATA
 WHERE (GENOTYPE & 1 OR GENOTYPE & 4) AND !(GENOTYPE & 2)
+# 2진수 계산 
+# (1번 형질 : 1 -> 1, 3번 형질 : 4 -> 100)를 포함하며
+# (2번 형질 : 2 -> 10)을 포함하지 않는 대장균
