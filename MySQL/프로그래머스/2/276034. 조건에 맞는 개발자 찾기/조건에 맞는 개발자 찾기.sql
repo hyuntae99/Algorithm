496c54a9399811bf6b962dee681d031bd4f55ad5
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-10-26 15:33:44 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,11 @@
+SELECT 
+    ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM 
+    DEVELOPERS
+WHERE 
+    # 서브쿼리에 속하는 ID만
+    ID IN (SELECT DISTINCT D.ID # 중복 제거
+           FROM DEVELOPERS D 
+           JOIN SKILLCODES S ON D.SKILL_CODE & S.CODE = S.CODE # 비트 연산
+           WHERE S.NAME = 'C#' OR S.NAME = 'Python')
+ORDER BY ID
