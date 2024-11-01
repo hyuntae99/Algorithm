1af2cb02ec42cb707dce999ecfc7a587627abc73
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-11-01 15:17:54 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,14 +1,24 @@
+# 백분위로 순서를 부여
+WITH A
+    AS (SELECT 
+            *, PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS PCT
+        FROM 
+            ECOLI_DATA)
+            
 SELECT 
-    A.ID, 
+    E.ID,
     CASE
-        WHEN B.PCT > 0.75 THEN 'LOW'
-        WHEN B.PCT > 0.5 THEN 'MEDIUM'
-        WHEN B.PCT > 0.25 THEN 'HIGH'
-        ELSE'CRITICAL'
+        WHEN A.PCT > 0.75 THEN 'LOW'
+        WHEN A.PCT > 0.5 THEN 'MEDIUM'
+        WHEN A.PCT > 0.25 THEN 'HIGH'
+        ELSE 'CRITICAL'
     END AS COLONY_NAME
 FROM 
-    ECOLI_DATA A
-JOIN 
-    (SELECT ID, PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS PCT
-     FROM ECOLI_DATA) B ON A.ID = B.ID
-ORDER BY A.ID;
+    ECOLI_DATA E
+JOIN
+    A
+ON 
+    E.ID = A.ID
+ORDER BY
+    E.ID;
+    
