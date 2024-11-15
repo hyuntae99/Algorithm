94a5e0928358fedae42a83e004bd66fc1ef63f8a
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2024-11-15 20:15:05 +0900

    [D2] Title: 스도쿠 검증, Time: 104 ms, Memory: 44,796 KB -BaekjoonHub

---
+++ Python/SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -0,0 +1,35 @@
+def check1(arr):
+    # 세로 확인
+    for i in range(9):
+        if set(arr[j][i] for j in range(9)) != {1, 2, 3, 4, 5, 6, 7, 8, 9}:
+            return False
+
+    # 가로 확인
+    for row in arr:
+        if set(row) != {1, 2, 3, 4, 5, 6, 7, 8, 9}:
+            return False
+
+    return True
+
+def check2(arr):
+    points = [(0,0), (3,0), (6,0),
+              (0,3), (3,3), (6,3),
+              (0,6), (3,6), (6,6)]
+
+    for si, sj in points:
+        s = set()
+        for i in range(si, si+3):
+            for j in range(sj, sj+3):
+                s.add(arr[i][j])
+
+        if s != {1, 2, 3, 4, 5, 6, 7, 8, 9}:
+            return False
+
+    return True
+
+T = int(input())
+for test_case in range(1, T+1):
+    arr = [list(map(int, input().split())) for _ in range(9)]
+
+    ans = 1 if check1(arr) and check2(arr) else 0
+    print(f'#{test_case} {ans}')
